bff140acc6694007c1feb6d6ddbb8febb448d851
saadahmedscse <saadahmedscse@gmail.com>
2023-09-17 05:27:09 +0600

    created product request dto

---
+++ ProductService/src/main/java/com/saadahmedev/productservice/dto/ProductRequest.java
@@ -0,0 +1,15 @@
+package com.saadahmedev.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private String title;
+    private String description;
+    private Double price;
+    private Integer discount;
+}
