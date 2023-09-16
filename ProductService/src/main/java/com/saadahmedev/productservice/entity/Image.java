208d4a9b3dc01da32cdebe3ba94686052dc1ebac
saadahmedscse <saadahmedscse@gmail.com>
2023-09-17 05:28:18 +0600

    created image entity

---
+++ ProductService/src/main/java/com/saadahmedev/productservice/entity/Image.java
@@ -0,0 +1,21 @@
+package com.saadahmedev.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String filename;
+    @Lob
+    private byte[] imageData;
+}
