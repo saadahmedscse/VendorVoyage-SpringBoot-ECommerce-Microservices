8d0da83e145c66892e1c0dc41121ab4261b5c8d8
saadahmedscse <saadahmedscse@gmail.com>
2023-09-17 05:27:18 +0600

    created product jpa repository

---
+++ ProductService/src/main/java/com/saadahmedev/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.saadahmedev.productservice.repository;
+
+import com.saadahmedev.productservice.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {}
