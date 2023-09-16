0458493daaaf793dd5d1f7e2c2ce282b83eafd59
saadahmedscse <saadahmedscse@gmail.com>
2023-09-17 05:26:55 +0600

    created product service

---
+++ ProductService/src/main/java/com/saadahmedev/productservice/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.saadahmedev.productservice.service;
+
+import com.saadahmedev.productservice.dto.ProductRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+public interface ProductService {
+
+    ResponseEntity<?> createProduct(List<MultipartFile> images, ProductRequest productRequest);
+
+    ResponseEntity<?> updateProduct(long id, List<MultipartFile> images, ProductRequest productRequest);
+
+    ResponseEntity<?> getProducts();
+
+    ResponseEntity<?> getProduct(long id);
+
+    ResponseEntity<?> deleteProduct(long id);
+}
