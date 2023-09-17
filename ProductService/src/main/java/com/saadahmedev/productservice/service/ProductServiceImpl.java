2e6b778b05a2ab09b5fd69d6e7fde244332f241f
saadahmedscse <saadahmedscse@gmail.com>
2023-09-17 23:55:58 +0600

    map project to project wrapper class

--- ProductService/src/main/java/com/saadahmedev/productservice/service/ProductServiceImpl.java
+++ ProductService/src/main/java/com/saadahmedev/productservice/service/ProductServiceImpl.java
@@ -1,9 +1,12 @@
 package com.saadahmedev.productservice.service;
 
 import com.saadahmedev.productservice.dto.ApiResponse;
+import com.saadahmedev.productservice.dto.InventoryResponse;
 import com.saadahmedev.productservice.dto.ProductRequest;
+import com.saadahmedev.productservice.dto.ProductWrapper;
 import com.saadahmedev.productservice.entity.Image;
 import com.saadahmedev.productservice.entity.Product;
+import com.saadahmedev.productservice.feing.InventoryService;
 import com.saadahmedev.productservice.repository.ImageRepository;
 import com.saadahmedev.productservice.repository.ProductRepository;
 import com.saadahmedev.productservice.util.DateTimeUtil;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -31,6 +35,9 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ImageUploadService imageUploadService;
+
+    @Autowired
+    private InventoryService inventoryService;
 
     @Override
     public ResponseEntity<?> createProduct(List<MultipartFile> images, ProductRequest productRequest) {
@@ -58,7 +65,9 @@ public class ProductServiceImpl implements ProductService {
 
         try {
             imageRepository.saveAll(imageData);
-            productRepository.save(product);
+            Product p = productRepository.save(product);
+            ResponseEntity<ApiResponse> apiResponse = inventoryService.createProduct(p.getId(), productRequest.getStock());
+            if (!apiResponse.getStatusCode().isSameCodeAs(HttpStatus.CREATED)) return apiResponse;
 
             return new ResponseEntity<>(new ApiResponse(true, "Product added successfully"), HttpStatus.CREATED);
         } catch (Exception e) {
@@ -73,7 +82,29 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ResponseEntity<?> getProducts() {
-        return new ResponseEntity<>(productRepository.findAll(), HttpStatus.OK);
+        List<Product> products = productRepository.findAll();
+        List<ProductWrapper> productWrappers = new ArrayList<>();
+
+        for (Product p : products) {
+            InventoryResponse inventoryResponse = inventoryService.getProduct(p.getId()).getBody();
+
+            productWrappers.add(
+                    ProductWrapper.builder()
+                            .id(p.getId())
+                            .title(p.getTitle())
+                            .description(p.getDescription())
+                            .price(p.getPrice())
+                            .discount(p.getDiscount())
+                            .images(p.getImages())
+                            .isAvailable(inventoryResponse.getProductCount() > 0)
+                            .inStock(inventoryResponse.getProductCount())
+                            .createdAt(p.getCreatedAt())
+                            .updatedAt(p.getUpdatedAt())
+                            .build()
+            );
+        }
+
+        return new ResponseEntity<>(productWrappers, HttpStatus.OK);
     }
 
     @Override
