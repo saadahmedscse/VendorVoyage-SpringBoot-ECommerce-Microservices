d230dae6e4eedc993a1f2890afadf4fda5b96104
saadahmedscse <saadahmedscse@gmail.com>
2023-09-20 04:49:27 +0600

    created token entity

---
+++ AuthService/src/main/java/com/saadahmedev/authservice/entity/Token.java
@@ -0,0 +1,20 @@
+package com.saadahmedev.authservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "table_token")
+public class Token {
+    @Id
+    private String token;
+}
