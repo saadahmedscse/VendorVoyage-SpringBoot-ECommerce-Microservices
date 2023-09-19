6c745315148960bfa3e94f531af1dedf5e026516
saadahmedscse <saadahmedscse@gmail.com>
2023-09-20 04:08:11 +0600

    created user entity

---
+++ AuthService/src/main/java/com/saadahmedev/authservice/entity/User.java
@@ -0,0 +1,22 @@
+package com.saadahmedev.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "table_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+}
