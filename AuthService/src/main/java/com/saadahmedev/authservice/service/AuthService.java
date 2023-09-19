d36ab7af68fd6fd2d92c8c4c622749248c14bb00
saadahmedscse <saadahmedscse@gmail.com>
2023-09-20 04:07:07 +0600

    added create, login, logout, validate-token functions

---
+++ AuthService/src/main/java/com/saadahmedev/authservice/service/AuthService.java
@@ -0,0 +1,19 @@
+package com.saadahmedev.authservice.service;
+
+import com.saadahmedev.authservice.dto.CreateAccountRequest;
+import com.saadahmedev.authservice.dto.LoginRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AuthService {
+
+    ResponseEntity<?> createAccount(CreateAccountRequest createAccountRequest);
+
+    ResponseEntity<?> login(LoginRequest loginRequest);
+
+    ResponseEntity<?> logout(HttpServletRequest request);
+
+    ResponseEntity<?> validateToken(String bearerToken);
+}
