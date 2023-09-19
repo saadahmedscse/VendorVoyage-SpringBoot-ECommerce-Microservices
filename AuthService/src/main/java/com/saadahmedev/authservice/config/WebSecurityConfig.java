0410c716e5c827167928db7a8ffaba4a3f6b1c92
saadahmedscse <saadahmedscse@gmail.com>
2023-09-20 04:08:40 +0600

    added request matchers

---
+++ AuthService/src/main/java/com/saadahmedev/authservice/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.saadahmedev.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/create").permitAll()
+                        .requestMatchers("/api/auth/validate-token/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
