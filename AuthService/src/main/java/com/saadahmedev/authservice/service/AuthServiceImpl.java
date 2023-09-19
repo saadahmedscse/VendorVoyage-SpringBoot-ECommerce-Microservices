4c4d06c6c95879335a6e21e504881dd803a67fc0
saadahmedscse <saadahmedscse@gmail.com>
2023-09-20 04:47:56 +0600

    added login and token generate functionality

--- AuthService/src/main/java/com/saadahmedev/authservice/service/AuthServiceImpl.java
+++ AuthService/src/main/java/com/saadahmedev/authservice/service/AuthServiceImpl.java
@@ -3,13 +3,21 @@ package com.saadahmedev.authservice.service;
 import com.saadahmedev.authservice.dto.ApiResponse;
 import com.saadahmedev.authservice.dto.CreateAccountRequest;
 import com.saadahmedev.authservice.dto.LoginRequest;
+import com.saadahmedev.authservice.dto.LoginResponse;
+import com.saadahmedev.authservice.entity.Token;
 import com.saadahmedev.authservice.entity.User;
+import com.saadahmedev.authservice.repository.TokenRepository;
 import com.saadahmedev.authservice.repository.UserRepository;
+import com.saadahmedev.authservice.util.JwtUtil;
 import com.saadahmedev.authservice.util.RequestValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +32,20 @@ public class AuthServiceImpl implements AuthService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    private String jwtToken = null;
 
     @Override
     public ResponseEntity<?> createAccount(CreateAccountRequest createAccountRequest) {
@@ -46,7 +68,19 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public ResponseEntity<?> login(LoginRequest loginRequest) {
-        return null;
+        ResponseEntity<?> validationResult = requestValidator.isLoginRequestvalid(loginRequest);
+        if (validationResult.getStatusCode().isSameCodeAs(HttpStatus.BAD_REQUEST)) return validationResult;
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
+        jwtToken = jwtUtil.generateToken(userDetails);
+
+        try {
+            tokenRepository.save(new Token(jwtToken));
+            return new ResponseEntity<>(new LoginResponse(true, "Logged in successfully", jwtToken), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse(false, e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
