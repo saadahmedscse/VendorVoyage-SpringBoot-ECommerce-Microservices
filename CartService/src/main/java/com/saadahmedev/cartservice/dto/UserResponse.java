c2d78a8b0dbca59694e885b9bce5c8d9bb2d860b
saadahmedscse <saadahmedscse@gmail.com>
2023-09-21 05:18:17 +0600

    created user reponse dto

---
+++ CartService/src/main/java/com/saadahmedev/cartservice/dto/UserResponse.java
@@ -0,0 +1,16 @@
+package com.saadahmedev.cartservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private long id;
+    private String name;
+    private String email;
+}
