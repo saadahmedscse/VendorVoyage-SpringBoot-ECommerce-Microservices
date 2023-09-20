d84e9bed0d4e627b04080b35bb0e177cc2303912
saadahmedscse <saadahmedscse@gmail.com>
2023-09-21 05:21:24 +0600

    get user via feign client

--- CartService/src/main/java/com/saadahmedev/cartservice/feing/AuthService.java
+++ CartService/src/main/java/com/saadahmedev/cartservice/feing/AuthService.java
@@ -2,9 +2,11 @@ package com.saadahmedev.cartservice.feing;
 
 import com.saadahmedev.cartservice.dto.ApiResponse;
 import com.saadahmedev.cartservice.dto.Token;
+import com.saadahmedev.cartservice.dto.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,4 +16,7 @@ public interface AuthService {
 
     @PostMapping("api/auth/validate-token")
     ResponseEntity<ApiResponse> validateToken(@RequestBody Token token);
+
+    @GetMapping("api/auth/user")
+    ResponseEntity<UserResponse> getUser(@RequestBody Token token);
 }
