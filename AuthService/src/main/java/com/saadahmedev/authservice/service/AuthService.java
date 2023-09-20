24e743771d6dea570a46fa683edb21ee7130ebc0
saadahmedscse <saadahmedscse@gmail.com>
2023-09-21 05:21:34 +0600

    added get user method

--- AuthService/src/main/java/com/saadahmedev/authservice/service/AuthService.java
+++ AuthService/src/main/java/com/saadahmedev/authservice/service/AuthService.java
@@ -3,6 +3,7 @@ package com.saadahmedev.authservice.service;
 import com.saadahmedev.authservice.dto.ApiResponse;
 import com.saadahmedev.authservice.dto.CreateAccountRequest;
 import com.saadahmedev.authservice.dto.LoginRequest;
+import com.saadahmedev.authservice.dto.UserResponse;
 import com.saadahmedev.authservice.entity.Token;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
@@ -18,4 +19,6 @@ public interface AuthService {
     ResponseEntity<?> logout(HttpServletRequest request);
 
     ResponseEntity<ApiResponse> validateToken(Token token);
+
+    ResponseEntity<UserResponse> getUser(Token token);
 }
