63a4544f5e6d6179f8af012949efac45cfbc71a3
saadahmedscse <saadahmedscse@gmail.com>
2023-09-21 05:35:18 +0600

    get cart items by user id

--- CartService/src/main/java/com/saadahmedev/cartservice/repository/CartRepository.java
+++ CartService/src/main/java/com/saadahmedev/cartservice/repository/CartRepository.java
@@ -4,5 +4,10 @@ import com.saadahmedev.cartservice.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {}
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    List<Cart> findCartByUserId(long userId);
+}
