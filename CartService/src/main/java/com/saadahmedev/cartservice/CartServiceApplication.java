bed6f3232c128735cc724dcc4f37727ed2c829d8
saadahmedscse <saadahmedscse@gmail.com>
2023-09-21 04:31:39 +0600

    enabled feign client

--- CartService/src/main/java/com/saadahmedev/cartservice/CartServiceApplication.java
+++ CartService/src/main/java/com/saadahmedev/cartservice/CartServiceApplication.java
@@ -2,8 +2,10 @@ package com.saadahmedev.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CartServiceApplication {
 
     public static void main(String[] args) {
