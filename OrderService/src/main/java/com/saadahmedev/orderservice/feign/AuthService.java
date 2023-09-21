c5e45995e791d7d53bb7b683b08c2f04bca94f75
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 03:05:23 +0600

    validate token from auth service

---
+++ OrderService/src/main/java/com/saadahmedev/orderservice/feign/AuthService.java
@@ -0,0 +1,22 @@
+package com.saadahmedev.orderservice.feign;
+
+import com.saadahmedev.orderservice.dto.ApiResponse;
+import com.saadahmedev.orderservice.dto.Token;
+import com.saadahmedev.orderservice.dto.UserResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@FeignClient("AUTH-SERVICE")
+public interface AuthService {
+
+    @PostMapping("api/auth/validate-token")
+    ResponseEntity<ApiResponse> validateToken(@RequestBody Token token);
+
+    @GetMapping("api/auth/user")
+    ResponseEntity<UserResponse> getUser(@RequestBody Token token);
+}
