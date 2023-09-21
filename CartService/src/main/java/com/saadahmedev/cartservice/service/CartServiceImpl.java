a7e00c96379fdc1cef7b75b5e92350a8b6a82c00
saadahmedscse <saadahmedscse@gmail.com>
2023-09-21 06:00:02 +0600

    update item count if existed, remove item, delete cart

--- CartService/src/main/java/com/saadahmedev/cartservice/service/CartServiceImpl.java
+++ CartService/src/main/java/com/saadahmedev/cartservice/service/CartServiceImpl.java
@@ -8,10 +8,13 @@ import com.saadahmedev.cartservice.feing.AuthService;
 import com.saadahmedev.cartservice.repository.CartRepository;
 import com.saadahmedev.cartservice.util.DateTimeUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CartServiceImpl implements CartService {
@@ -26,15 +29,29 @@ public class CartServiceImpl implements CartService {
     public ResponseEntity<?> addProduct(HttpServletRequest request, Cart cart) {
         long userId = getUserId(request);
         if (userId == -1) return userNotFound();
-
+        Optional<Cart> optionalCart = cartRepository.findCartByUserIdAndProductId(userId, cart.getProductId());
         String creationTime = DateTimeUtil.getCurrentDateTime();
-        Cart newCart = Cart.builder()
-                .productId(cart.getProductId())
-                .userId(userId)
-                .itemCount(cart.getItemCount())
-                .createdAt(creationTime)
-                .updatedAt(creationTime)
-                .build();
+        Cart newCart;
+
+        if (optionalCart.isEmpty()) {
+            newCart = Cart.builder()
+                    .productId(cart.getProductId())
+                    .userId(userId)
+                    .itemCount(cart.getItemCount())
+                    .createdAt(creationTime)
+                    .updatedAt(creationTime)
+                    .build();
+        } else {
+            Cart c = optionalCart.get();
+            newCart = Cart.builder()
+                    .id(c.getId())
+                    .productId(c.getProductId())
+                    .userId(c.getUserId())
+                    .itemCount(cart.getItemCount() + c.getItemCount())
+                    .createdAt(c.getCreatedAt())
+                    .updatedAt(creationTime)
+                    .build();
+        }
 
         try {
             cartRepository.save(newCart);
@@ -60,8 +77,31 @@ public class CartServiceImpl implements CartService {
     }
 
     @Override
+    @Transactional
     public ResponseEntity<?> removeItem(HttpServletRequest request, long productId) {
-        return null;
+        long userId = getUserId(request);
+        Optional<Cart> optionalCart = cartRepository.findCartByUserIdAndProductId(userId, productId);
+        if (optionalCart.isEmpty()) return new ResponseEntity<>(new ApiResponse(false, "Item not found"), HttpStatus.BAD_REQUEST);
+
+        try {
+            cartRepository.deleteCartByUserIdAndProductId(userId, productId);
+            return new ResponseEntity<>(new ApiResponse(true, "Item removed from cart successfully"), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse(false, e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @Transactional
+    public ResponseEntity<?> deleteCart(HttpServletRequest request) {
+        long userId = getUserId(request);
+
+        try {
+            cartRepository.deleteAllByUserId(userId);
+            return new ResponseEntity<>(new ApiResponse(true, "Cart deleted successfully"), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse(false, e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
