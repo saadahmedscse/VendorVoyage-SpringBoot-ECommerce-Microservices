b0065643a16d8d06a5950473c52d37fb460d2335
saadahmedscse <saadahmedscse@gmail.com>
2023-09-21 06:00:35 +0600

    added custom JPQLs

--- CartService/src/main/java/com/saadahmedev/cartservice/repository/CartRepository.java
+++ CartService/src/main/java/com/saadahmedev/cartservice/repository/CartRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     List<Cart> findCartByUserId(long userId);
+
+    Optional<Cart> findCartByUserIdAndProductId(long userId, long productId);
+
+    void deleteCartByUserIdAndProductId(long userId, long productId);
+
+    void deleteAllByUserId(long userId);
 }
