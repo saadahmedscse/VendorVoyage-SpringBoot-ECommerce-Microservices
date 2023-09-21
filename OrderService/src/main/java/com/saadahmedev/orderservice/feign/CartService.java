f2f3e7186ada2c6cdc60a56efc8cdf0e9560d7de
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 04:22:56 +0600

    added delete cart by user id handler

--- OrderService/src/main/java/com/saadahmedev/orderservice/feign/CartService.java
+++ OrderService/src/main/java/com/saadahmedev/orderservice/feign/CartService.java
@@ -1,9 +1,11 @@
 package com.saadahmedev.orderservice.feign;
 
+import com.saadahmedev.orderservice.dto.ApiResponse;
 import com.saadahmedev.orderservice.dto.product.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -15,4 +17,7 @@ public interface CartService {
 
     @GetMapping("api/cart/{id}")
     ResponseEntity<List<Product>> getCartItems(@PathVariable long id);
+
+    @DeleteMapping("api/cart/remove/{id}")
+    ResponseEntity<ApiResponse> removeCartByUserId(@PathVariable long id);
 }
