e42632593089c2fae57a021b747d91f74301af2f
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 03:05:08 +0600

    created OrderService

---
+++ OrderService/src/main/java/com/saadahmedev/orderservice/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.saadahmedev.orderservice.service;
+
+import com.saadahmedev.orderservice.entity.ShippingDetails;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface OrderService {
+
+    ResponseEntity<?> createOrder(HttpServletRequest request, ShippingDetails shippingDetails);
+
+    ResponseEntity<?> getOrders(HttpServletRequest request);
+
+    ResponseEntity<?> getOrder(HttpServletRequest request, String id);
+}
