21769f1cb71a713f22bc1938ff48dae0b50ea71a
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 03:07:02 +0600

    added required handlers

---
+++ OrderService/src/main/java/com/saadahmedev/orderservice/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.saadahmedev.orderservice.controller;
+
+import com.saadahmedev.orderservice.entity.ShippingDetails;
+import com.saadahmedev.orderservice.service.OrderService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<?> createOrder(HttpServletRequest request, @RequestBody ShippingDetails shippingDetails) {
+        return orderService.createOrder(request, shippingDetails);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getOrders(HttpServletRequest request) {
+        return orderService.getOrders(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOrder(HttpServletRequest request, @PathVariable String id) {
+        return orderService.getOrder(request, id);
+    }
+}
