a9e844afb6d83e43370e69ba7c967f71da330f00
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 03:06:06 +0600

    created product dto

---
+++ CartService/src/main/java/com/saadahmedev/cartservice/dto/product/Product.java
@@ -0,0 +1,24 @@
+package com.saadahmedev.cartservice.dto.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private long id;
+    private String title;
+    private String description;
+    private double price;
+    private int discount;
+    private int itemCount;
+    private List<Image> images;
+    private String createdAt;
+    private String updatedAt;
+}
