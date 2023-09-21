2ffc7917650294dbaf18f1b57c7c3bcd8bd64d36
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 04:08:16 +0600

    get order and order list functionality implemented

--- OrderService/src/main/java/com/saadahmedev/orderservice/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/saadahmedev/orderservice/service/OrderServiceImpl.java
@@ -4,23 +4,25 @@ import com.saadahmedev.orderservice.dto.ApiResponse;
 import com.saadahmedev.orderservice.dto.OrderPlaceResponse;
 import com.saadahmedev.orderservice.dto.Token;
 import com.saadahmedev.orderservice.dto.UserResponse;
+import com.saadahmedev.orderservice.dto.orderResponse.OrderResponse;
 import com.saadahmedev.orderservice.dto.product.Product;
 import com.saadahmedev.orderservice.entity.*;
 import com.saadahmedev.orderservice.feign.AuthService;
 import com.saadahmedev.orderservice.feign.CartService;
+import com.saadahmedev.orderservice.feign.ProductService;
 import com.saadahmedev.orderservice.repository.OrderRepository;
 import com.saadahmedev.orderservice.repository.PaymentStatusRepository;
+import com.saadahmedev.orderservice.repository.ProductCountRepository;
 import com.saadahmedev.orderservice.repository.ShippingDetailsRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -35,10 +37,18 @@ public class OrderServiceImpl implements OrderService {
     private ShippingDetailsRepository shippingDetailsRepository;
 
     @Autowired
+    private ProductCountRepository productCountRepository;
+
+    @Autowired
     private AuthService authService;
 
     @Autowired
     private CartService cartService;
+
+    @Autowired
+    private ProductService productService;
+
+    private Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Override
     public ResponseEntity<?> createOrder(HttpServletRequest request, ShippingDetails shippingDetails) {
@@ -46,19 +56,24 @@ public class OrderServiceImpl implements OrderService {
         if (userId == -1) return userNotFound();
 
         List<Product> productList = cartService.getCartItems(userId).getBody();
-        List<Long> productIds = new ArrayList<>();
+        List<com.saadahmedev.orderservice.entity.Product> productCountList = new ArrayList<>();
 
         double totalPrice = 0;
         double grandTotal = 0;
 
         assert productList != null;
         for (Product product : productList) {
-            productIds.add(product.getId());
-            totalPrice += product.getPrice();
+            productCountList.add(
+                    com.saadahmedev.orderservice.entity.Product.builder()
+                            .productId(product.getId())
+                            .itemCount(product.getItemCount())
+                            .build()
+            );
+            totalPrice += (product.getPrice() * product.getItemCount());
             double discountPrice;
-            if (product.getDiscount() == 0) discountPrice = product.getPrice();
+            if (product.getDiscount() == 0) discountPrice = product.getPrice() * product.getItemCount();
             else {
-                discountPrice = product.getPrice() - (product.getPrice() * product.getDiscount()) / 100;
+                discountPrice = (product.getPrice() * product.getItemCount()) - (product.getPrice() * product.getItemCount() * product.getDiscount()) / 100;
             }
             grandTotal += discountPrice;
         }
@@ -76,7 +91,7 @@ public class OrderServiceImpl implements OrderService {
                 .totalPrice(totalPrice)
                 .discount(0)
                 .grandTotal(grandTotal)
-                .productIds(productIds)
+                .products(productCountList)
                 .deliveryStatus(DeliveryStatus.PENDING)
                 .shippingDetails(shippingDetails)
                 .paymentStatus(paymentStatus) //This will come from payment service using Kafka (SAGA Choreography Architecture)
@@ -85,6 +100,7 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         try {
+            productCountRepository.saveAll(productCountList);
             shippingDetailsRepository.save(shippingDetails);
             paymentStatusRepository.save(paymentStatus);
             Order savedOrder = orderRepository.save(order);
@@ -96,12 +112,54 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public ResponseEntity<?> getOrders(HttpServletRequest request) {
-        return null;
+        long userId = getUserId(request);
+        List<Order> orderList = orderRepository.findAllByUserId(userId);
+        if (orderList.isEmpty()) return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+
+        List<OrderResponse> orderResponseList = new ArrayList<>();
+        for (Order order : orderList) {
+            orderResponseList.add(mapToOrderResponse(order));
+        }
+
+        return new ResponseEntity<>(orderResponseList, HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<?> getOrder(HttpServletRequest request, String id) {
-        return null;
+    public ResponseEntity<?> getOrder(HttpServletRequest request, long id) {
+        long userId = getUserId(request);
+        Optional<Order> optionalOrder = orderRepository.findByIdAndUserId(id, userId);
+        if (optionalOrder.isEmpty()) return new ResponseEntity<>(new ApiResponse(false, "Order not found"), HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(mapToOrderResponse(optionalOrder.get()), HttpStatus.OK);
+    }
+
+    private OrderResponse mapToOrderResponse(Order order) {
+        Map<Long, Integer> productItemCount = new HashMap<>();
+        List<Long> productIds = new ArrayList<>();
+
+        for (com.saadahmedev.orderservice.entity.Product product : order.getProducts()) {
+            productItemCount.put(product.getProductId(), product.getItemCount());
+            productIds.add(product.getProductId());
+        }
+
+        List<Product> productList = productService.getCartProducts(productIds).getBody();
+        assert productList != null;
+        for (Product product : productList) {
+            product.setItemCount(productItemCount.get(product.getId()));
+        }
+
+        return OrderResponse.builder()
+                .id(order.getId())
+                .totalPrice(order.getTotalPrice())
+                .discount(order.getDiscount())
+                .grandTotal(order.getGrandTotal())
+                .products(productList)
+                .deliveryStatus(order.getDeliveryStatus())
+                .shippingDetails(order.getShippingDetails())
+                .paymentStatus(order.getPaymentStatus())
+                .createdAt(order.getCreatedAt())
+                .updatedAt(order.getUpdatedAt())
+                .build();
     }
 
     private long getUserId(HttpServletRequest request) {
