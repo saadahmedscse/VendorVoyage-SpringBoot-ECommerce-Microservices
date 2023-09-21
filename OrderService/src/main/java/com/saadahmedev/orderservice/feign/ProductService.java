61d2ec22ac5bfc87cf1c70f1daa8b2373eac70db
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 04:05:41 +0600

    get products from product-service

---
+++ OrderService/src/main/java/com/saadahmedev/orderservice/feign/ProductService.java
@@ -0,0 +1,18 @@
+package com.saadahmedev.orderservice.feign;
+
+import com.saadahmedev.orderservice.dto.product.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+@FeignClient("PRODUCT-SERVICE")
+public interface ProductService {
+
+    @PostMapping("api/product/cart")
+    ResponseEntity<List<Product>> getCartProducts(@RequestBody List<Long> productIds);
+}
