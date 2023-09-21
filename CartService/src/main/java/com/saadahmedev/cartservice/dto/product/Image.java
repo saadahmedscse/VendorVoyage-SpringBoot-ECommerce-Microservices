c9ac01971d2f79f6137c3574c8771c44f2b2b56e
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 03:07:20 +0600

    created image dto

---
+++ CartService/src/main/java/com/saadahmedev/cartservice/dto/product/Image.java
@@ -0,0 +1,15 @@
+package com.saadahmedev.cartservice.dto.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+    private Long id;
+    private String url;
+}
