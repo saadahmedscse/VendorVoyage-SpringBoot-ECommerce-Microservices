513954ce3d203b6bf9b09f8a8a9fa2948fd19784
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 04:07:00 +0600

    created product entity for item count

---
+++ OrderService/src/main/java/com/saadahmedev/orderservice/entity/Product.java
@@ -0,0 +1,23 @@
+package com.saadahmedev.orderservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long productId;
+    private int itemCount;
+}
