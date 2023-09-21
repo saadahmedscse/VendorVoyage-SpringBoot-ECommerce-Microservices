54c9985d7a0b4ee40228d66c97b0c589c1493393
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 04:08:43 +0600

    added custom JPQL's

--- OrderService/src/main/java/com/saadahmedev/orderservice/repository/OrderRepository.java
+++ OrderService/src/main/java/com/saadahmedev/orderservice/repository/OrderRepository.java
@@ -4,6 +4,13 @@ import com.saadahmedev.orderservice.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
+
+    List<Order> findAllByUserId(long userId);
+
+    Optional<Order> findByIdAndUserId(long id, long userId);
 }
