4f171382de9039804f93202d71a967b610df2d68
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 22:25:49 +0600

    removed service annotation

--- PaymentService/src/main/java/com/saadahmedev/paymentservice/service/OnMessageReceived.java
+++ PaymentService/src/main/java/com/saadahmedev/paymentservice/service/OnMessageReceived.java
@@ -1,9 +1,7 @@
 package com.saadahmedev.paymentservice.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface OnMessageReceived {
     void onEvent(String event) throws JsonProcessingException;
 }
