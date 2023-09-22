a1cb62ed9a9cacdf49320748a7c01b7aac6cce1d
saadahmedscse <saadahmedscse@gmail.com>
2023-09-22 22:25:37 +0600

    generation type from auto to identify

--- OrderService/src/main/java/com/saadahmedev/orderservice/entity/Order.java
+++ OrderService/src/main/java/com/saadahmedev/orderservice/entity/Order.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Table(name = "table_order")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @JsonIgnore
     private long userId;
