1768c49115f3312de83cf6de3f74e947bd7bff77
saadahmedscse <saadahmedscse@gmail.com>
2023-09-25 00:55:57 +0600

    added custom feign error decoder

---
+++ CartService/src/main/java/com/saadahmedev/cartservice/feing/FeignErrorDecoder.java
@@ -0,0 +1,34 @@
+package com.saadahmedev.cartservice.feing;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import feign.Response;
+import feign.Util;
+import feign.codec.ErrorDecoder;
+import lombok.SneakyThrows;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class FeignErrorDecoder implements ErrorDecoder {
+
+    @SneakyThrows
+    @Override
+    public Exception decode(String s, Response response) {
+        if (response.status() == 503) {
+            Map<String, Object> responseBody = parseResponseBody(response);
+            String message = (String) responseBody.get("message");
+
+            return new RuntimeException("SNAE_" + message); //Service Not Available Exception
+        }
+
+        return FeignException.errorStatus(s, response);
+    }
+
+    private Map<String, Object> parseResponseBody(Response response) throws IOException {
+        String responseBody = Util.toString(response.body().asReader());
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(responseBody, new TypeReference<>() {});
+    }
+}
